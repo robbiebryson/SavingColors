5e32bbe5d55019591e0953c53d6edeb8ae362f99
robbiebryson <65382229+robbiebryson@users.noreply.github.com>
2024-03-24 22:18:04 -0400

    Initial Commit

---
+++ SavingColors/SavingColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavingColorsApp.swift
+//  SavingColors
+//
+//  Created by Rob Bryson on 3/24/24.
+//
+
+import SwiftUI
+
+@main
+struct SavingColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
